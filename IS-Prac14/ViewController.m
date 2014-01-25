2fddb0104442efb9c2064760e6d32d63057b21ba
mits <mits.jp@gmail.com>
2014-01-25 15:27:56 +0900

    演習１４完了

--- IS-Prac14/ViewController.m
+++ IS-Prac14/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIImageView *sq;
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *singleTapSq;
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *doubleTapSq;
+
+- (IBAction)tapSq:(UITapGestureRecognizer *)sender;
+- (IBAction)dTapSq:(UITapGestureRecognizer *)sender;
+- (IBAction)routationSq:(UIRotationGestureRecognizer *)sender;
+
 @end
 
 @implementation ViewController
@@ -18,12 +26,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [_singleTapSq requireGestureRecognizerToFail:_doubleTapSq];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapSq:(UITapGestureRecognizer *)sender
+{
+    sender.view.center = CGPointMake(sender.view.center.x + 10, sender.view.center.y);
+}
+
+- (IBAction)dTapSq:(UITapGestureRecognizer *)sender
+{
+    if (CGAffineTransformIsIdentity(sender.view.transform)) {
+        sender.view.transform = CGAffineTransformMakeScale(3.0, 3.0);
+    } else {
+        sender.view.transform = CGAffineTransformIdentity;
+        sender.view.center = self.view.center;
+    }
+}
+
+- (IBAction)routationSq:(UIRotationGestureRecognizer *)sender
+{
+    _sq.transform = CGAffineTransformMakeRotation(sender.rotation);
 }
 
 @end
